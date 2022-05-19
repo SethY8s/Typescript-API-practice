04a77461fe123fe767ac8bda81c60d2c8f7f371d
Seth8656 <95386742+SethY8s@users.noreply.github.com>
2022-05-19 16:55:00 -0400

    fetched data from weather api and console.logged it

--- src/tsModules/fetch.ts
+++ src/tsModules/fetch.ts
@@ -1,9 +1,10 @@
-export {fetchData}
-import { apikey } from "./apikey"
+export { fetchData };
+import { apiKey } from './apikey';
 
-const fetchData = () => {
-
-console.log(apikey)
-
-
-}
+const fetchData = async () => {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=imperial`
+  )
+    .then((data) => data.json())
+    .then((el) => console.log(el));
+};
