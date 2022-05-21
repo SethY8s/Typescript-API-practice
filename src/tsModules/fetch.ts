345c527f335f8037c9b24dc4695a775fce995d91
Seth8656 <95386742+SethY8s@users.noreply.github.com>
2022-05-21 17:02:55 -0400

    playing with api routes to get correct data

--- src/tsModules/fetch.ts
+++ src/tsModules/fetch.ts
@@ -9,9 +9,21 @@ import { apiKey } from './apikey';
 //     .then((el) => console.log(el));
 // }; These two functions do the same thing
 
-const fetchData = async (input:string) => {
-  const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${apiKey}&units=imperial`)
-  let newData = await data.json()
-  console.log(newData.main.temp)
-  document.getElementById('insert').innerHTML = newData.main.temp
+const fetchData = async (input: string) => {
+  // const data = await fetch(
+  //   `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${apiKey}&cnt=7&units=imperial`
+  // );
+
+  // let TempAndCords = await data.json();
+
+  const data = await fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?q=${input}&cnt=3&appid=${apiKey}&units=imperial`
+  );
+
+  let TempAndCords = await data.json();
+
+ 
+
+  console.log(TempAndCords)
+  // document.getElementById('insert').innerHTML = TempAndCords.main.temp;
 };
