14b3936de6dc48fec89b3ad466a7fbfe576c47c0
Seth8656 <95386742+SethY8s@users.noreply.github.com>
2022-06-15 15:56:05 -0400

    added type gaurd so no longer gettig possible null error

--- src/tsModules/fetch.ts
+++ src/tsModules/fetch.ts
@@ -9,14 +9,14 @@ import { apiKey } from './apikey';
 //     .then((el) => console.log(el));
 // }; These two functions do the same thing
 
-const fetchData = async (input: string) => {
+const fetchData = async (input: string | null) => {
   const data = await fetch(
     `https://api.openweathermap.org/data/2.5/forecast?q=${input}&appid=${apiKey}&units=imperial`
   );
 
   let TempAndCords = await data.json();
 
-  const threeDayData = [];
+  const threeDayData : string[] = [];
 
   for (let i = 5; i < TempAndCords.list.length; i = i + 8) {
     threeDayData.push(TempAndCords.list[i].main.temp);
@@ -25,11 +25,20 @@ const fetchData = async (input: string) => {
   console.log(threeDayData);
 
   console.log(TempAndCords);
-  document.getElementById('insert').innerHTML = `${Math.ceil(
+
+ const insert = document.getElementById('insert')
+
+ if(insert !== null){
+  insert.innerHTML = `${Math.ceil(
     TempAndCords.list[0].main.temp
   )} F`;
+ }
 
-  document.getElementById('5day-insert').innerHTML = `${threeDayData.map(
+const fiveDayInsert = document.getElementById('5day-insert')
+
+if(fiveDayInsert !== null){
+  fiveDayInsert.innerHTML = `${threeDayData.map(
     (el) => `<p>${el}</p>`
   )}`;
+}
 };
